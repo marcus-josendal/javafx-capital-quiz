9e8c1de83e215e76a2e6b2d40d2e30a344431657
viking-v2 <31208951+viking-v2@users.noreply.github.com>
2018-04-05 12:40:59 +0200

    just checking if everything is working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Just checking if everything is working");
     }
 }
